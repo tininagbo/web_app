d5f4ce2c05dffaf04f54995358936a1f55c17a25
AGBO Tinin <tinin.agbo@imsp-uac.org>
2023-01-20 17:09:02 +0100

    first commit of the app

---
+++ website/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request
+auth = Blueprint('auth', __name__)
+
+@auth.route("/login", methods = ["GET", "POST"])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
+@auth.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+@auth.route("/sign-up", methods = ["GET", "POST"])
+def sign_up():
+    return render_template("sign-up.html")
